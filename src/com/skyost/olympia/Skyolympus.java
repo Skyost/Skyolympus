43d2896d0a237a8c5b1daf7bb9f8355dbd7a9935
Skyost <skyost@mail.com>
2013-10-28 13:11:33 +0100

    Bugs fixes and performance improvements.

--- src/com/skyost/olympia/Skyolympus.java
+++ src/com/skyost/olympia/Skyolympus.java
@@ -17,6 +17,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -28,88 +30,117 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Skyolympus extends JavaPlugin implements Listener{
 	
-	private HashMap<Player, String> Olympmod = new HashMap<Player, String>();
-	private ItemStack Zeus = new ItemStack(Material.GOLD_SWORD);
-	private ItemStack Poseidon = new ItemStack(Material.DIAMOND_AXE);
-	private ItemStack Hades = new ItemStack(Material.BLAZE_ROD);
-	private ItemStack Demeter = new ItemStack(Material.SEEDS);
-	private ItemStack Gaia = new ItemStack(Material.WOOD_SPADE);
-	private ItemStack Thanatos = new ItemStack(Material.STONE_HOE);
-	private ItemStack Ares = new ItemStack(Material.DIAMOND_SWORD);
+	private final HashMap<Player, String> Olympmod = new HashMap<Player, String>();
+	public static Inventory olympusInv;
 	
 	@Override
-	public void onEnable() {
+	public final void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		loadConfiguration();
 		loadOlympus();
 	}
 	
-	public void loadConfiguration() {
+	@Override
+	public final void onDisable() {
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			if(Olympmod.get(player) != null) {
+				Inventory inv = InvUtils.StringToInventory(Olympmod.get(player));
+				for(int i = 0; i != player.getInventory().getSize(); i++) {
+	    			if(player.getInventory().getItem(i) != null) {
+	    				player.getInventory().removeItem(player.getInventory().getItem(i));
+	    			}
+	    		}
+	    		for(int i = 0; i != inv.getSize(); i++) {
+	    			if(inv.getItem(i) != null) {
+	    				player.getInventory().addItem(inv.getItem(i));
+	    			}
+	    		}
+				Olympmod.remove(player);
+			}
+		}
+	}
+	
+	private final void loadConfiguration() {
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
 	
-	public void loadOlympus() {
-		ItemMeta ZeusMeta = Zeus.getItemMeta();
-		ZeusMeta.setDisplayName(ChatColor.GOLD + getConfig().getString("Items.1"));
-		Zeus.setItemMeta(ZeusMeta);
-		ItemMeta PoseidonMeta = Poseidon.getItemMeta();
-		PoseidonMeta.setDisplayName(ChatColor.AQUA + getConfig().getString("Items.2"));
-		Poseidon.setItemMeta(PoseidonMeta);
-		ItemMeta HadesMeta = Hades.getItemMeta();
-		HadesMeta.setDisplayName(ChatColor.RED + getConfig().getString("Items.3"));
-		Hades.setItemMeta(HadesMeta);
-		ItemMeta DemeterMeta = Demeter.getItemMeta();
-		DemeterMeta.setDisplayName(ChatColor.GREEN + getConfig().getString("Items.4"));
-		Demeter.setItemMeta(DemeterMeta);
-		ItemMeta GaiaMeta = Gaia.getItemMeta();
-		GaiaMeta.setDisplayName(ChatColor.DARK_GRAY + getConfig().getString("Items.5"));
-		Gaia.setItemMeta(GaiaMeta);
-		ItemMeta ThanatosMeta = Thanatos.getItemMeta();
-		ThanatosMeta.setDisplayName(ChatColor.BLACK + getConfig().getString("Items.6"));
-		Thanatos.setItemMeta(ThanatosMeta);
-		ItemMeta AresMeta = Ares.getItemMeta();
-		AresMeta.addEnchant(Enchantment.DAMAGE_ALL, 20, true);
-		AresMeta.addEnchant(Enchantment.DAMAGE_ARTHROPODS, 20, true);
-		AresMeta.addEnchant(Enchantment.DAMAGE_UNDEAD, 20, true);
-		AresMeta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 20, true);
-		AresMeta.addEnchant(Enchantment.KNOCKBACK, 20, true);
-		AresMeta.addEnchant(Enchantment.FIRE_ASPECT, 20, true);
-		AresMeta.addEnchant(Enchantment.DURABILITY, 20, true);
-		AresMeta.setDisplayName(ChatColor.YELLOW + getConfig().getString("Items.7"));
-		Ares.setItemMeta(AresMeta);
+	private final void loadOlympus() {
+		olympusInv = Bukkit.createInventory(null, InventoryType.PLAYER);
+		ItemStack olympusIE = new ItemStack(Material.GOLD_SWORD); // Zeus
+		ItemMeta olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.GOLD + getConfig().getString("Items.1"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.DIAMOND_AXE); // Poseidon
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.AQUA + getConfig().getString("Items.2"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.BLAZE_ROD); // Hades
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.RED + getConfig().getString("Items.3"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.SEEDS); // Demeter
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.GREEN + getConfig().getString("Items.4"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.WOOD_SPADE); // Gaia
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.DARK_GRAY + getConfig().getString("Items.5"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.STONE_HOE); // Thanatos
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.setDisplayName(ChatColor.BLACK + getConfig().getString("Items.6"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
+		olympusIE = new ItemStack(Material.DIAMOND_SWORD); // Ares
+		olympusMeta = olympusIE.getItemMeta();
+		olympusMeta.addEnchant(Enchantment.DAMAGE_ALL, 20, true);
+		olympusMeta.addEnchant(Enchantment.DAMAGE_ARTHROPODS, 20, true);
+		olympusMeta.addEnchant(Enchantment.DAMAGE_UNDEAD, 20, true);
+		olympusMeta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 20, true);
+		olympusMeta.addEnchant(Enchantment.KNOCKBACK, 20, true);
+		olympusMeta.addEnchant(Enchantment.FIRE_ASPECT, 20, true);
+		olympusMeta.addEnchant(Enchantment.DURABILITY, 20, true);
+		olympusMeta.setDisplayName(ChatColor.YELLOW + getConfig().getString("Items.7"));
+		olympusIE.setItemMeta(olympusMeta);
+		olympusInv.addItem(olympusIE);
 	}
 	
-	@SuppressWarnings("deprecation")
+	@SuppressWarnings({ "incomplete-switch", "deprecation" })
 	@EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    public final void onPlayerInteract(final PlayerInteractEvent event) {
     	if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
     		Player player = event.getPlayer();
     		if(Olympmod.get(player) != null) {
     			Location loc = event.getClickedBlock().getLocation();
-    			switch(player.getItemInHand().getTypeId()) {
-    			case 283:
+    			switch(player.getItemInHand().getType()) {
+    			case GOLD_SWORD:
     				player.getWorld().strikeLightning(loc);
     				player.getWorld().createExplosion(loc, 15.0F, false);
     				break;
-    			case 279:
+    			case DIAMOND_AXE:
     				player.getTargetBlock(null, 100).setType(Material.WATER);
     				player.getWorld().createExplosion(loc.getX(), loc.getY(), loc.getZ(), 4.0F, false, false);
     				break;
-    			case 369:
+    			case BLAZE_ROD:
     				player.getWorld().createExplosion(loc, 10.0F, true);
     				event.getClickedBlock().setType(Material.LAVA);
     				break;
-    			case 295:
+    			case SEEDS:
     				player.getWorld().generateTree(loc, TreeType.TREE);
     				player.getTargetBlock(null, 100).setType(Material.GRASS);
     				break;
-    			case 269:
+    			case WOOD_SPADE:
     				for(int i = event.getClickedBlock().getY(); i != 0; i--) {
     					player.getWorld().getBlockAt(event.getClickedBlock().getX(), i, event.getClickedBlock().getZ()).setType(Material.AIR);
     				}
     				break;
-    			case 291:
+    			case STONE_HOE:
     				Player[] online = getServer().getOnlinePlayers();
     				int random = new Random().nextInt(getServer().getOnlinePlayers().length);
     				if(online[random] != player) {
@@ -128,23 +159,23 @@ public class Skyolympus extends JavaPlugin implements Listener{
     	}
 	}
 	
-	@SuppressWarnings("deprecation")
+	@SuppressWarnings("incomplete-switch")
 	@EventHandler
-	private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+	private final void onPlayerInteractEntity(final PlayerInteractEntityEvent event) {
     	Player player = event.getPlayer();
     	if(Olympmod.get(player) != null) {
     		Entity entity = event.getRightClicked();
-    		switch(player.getItemInHand().getTypeId()) {
-    		case 369:
+    		switch(player.getItemInHand().getType()) {
+    		case BLAZE_ROD:
         		entity.setFireTicks(entity.getMaxFireTicks());
         		event.setCancelled(true);
     			break;
-    		case 295:
+    		case SEEDS:
     			entity.getWorld().dropItemNaturally(entity.getLocation(), new ItemStack(Material.SAPLING));
-    			entity.teleport(entity.getLocation().add(0, 100, 0));
+    			entity.teleport(entity.getLocation().add(0, -10, 0));
     			event.setCancelled(true);
     			break;
-			case 269:
+			case WOOD_SPADE:
 				entity.teleport(new Location(player.getWorld(), 0.0, -10.0, 0.0));
 				event.setCancelled(true);
 				break;
@@ -154,7 +185,7 @@ public class Skyolympus extends JavaPlugin implements Listener{
 	
 	@SuppressWarnings("deprecation")
 	@EventHandler
-	private void onPlayerDeath(PlayerDeathEvent event) {
+	private final void onPlayerDeath(final PlayerDeathEvent event) {
 		Player player = event.getEntity();
 		try {
 			if(Olympmod.get(player) != null) {
@@ -203,6 +234,13 @@ public class Skyolympus extends JavaPlugin implements Listener{
 		}
 	}
 	
+	@EventHandler
+	private void onPlayerDropItem(PlayerDropItemEvent event) {
+		if(Olympmod.get(event.getPlayer()) != null) {
+			event.setCancelled(true);
+		}
+	}
+	
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
         Player player = null;
@@ -213,45 +251,39 @@ public class Skyolympus extends JavaPlugin implements Listener{
             	if(Olympmod.get(player) != null) {
             		try {
 	            		Inventory inv = InvUtils.StringToInventory(Olympmod.get(player));
-	            		for(int i = 0; i != player.getInventory().getSize(); i++) {
-	            			if(player.getInventory().getItem(i) != null) {
-	            				player.getInventory().removeItem(player.getInventory().getItem(i));
-	            			}
-	            		}
-	            		for(int i = 0; i != inv.getSize(); i++) {
-	            			if(inv.getItem(i) != null) {
-	            				player.getInventory().addItem(inv.getItem(i));
-	            			}
-	            		}
+            			for(ItemStack ie : player.getInventory().getContents()) {
+            				if(ie != null) {
+            					player.getInventory().removeItem(ie);
+            				}
+            			}
+            			for(ItemStack ie : inv.getContents()) {
+            				if(ie != null) {
+            					player.getInventory().addItem(ie);
+            				}
+            			}
 	            		player.updateInventory();
 	            		Olympmod.remove(player);
 	            		sender.sendMessage(ChatColor.RED + getConfig().getString("Messages.2"));
             		}
             		catch(Exception ex) {
             			sender.sendMessage(ChatColor.RED + getConfig().getString("Messages.4"));
-            			for(int i = 0; i != player.getInventory().getSize(); i++) {
-                			if(player.getInventory().getItem(i) != null) {
-                				player.getInventory().removeItem(player.getInventory().getItem(i));
-                			}
-                		}
+            			for(ItemStack ie : player.getInventory().getContents()) {
+            				if(ie != null) {
+            					player.getInventory().removeItem(ie);
+            				}
+            			}
 	            		player.updateInventory();
 	            		Olympmod.remove(player);
             		}
             	}
             	else {
             		Olympmod.put(player, InvUtils.InventoryToString(player.getInventory()));
-            		for(int i = 0; i != player.getInventory().getSize(); i++) {
-            			if(player.getInventory().getItem(i) != null) {
-            				player.getInventory().removeItem(player.getInventory().getItem(i));
-            			}
-            		}
-            		player.getInventory().addItem(Zeus);
-            		player.getInventory().addItem(Poseidon);
-            		player.getInventory().addItem(Hades);
-            		player.getInventory().addItem(Demeter);
-            		player.getInventory().addItem(Gaia);
-            		player.getInventory().addItem(Thanatos);
-            		player.getInventory().addItem(Ares);
+        			for(ItemStack ie : player.getInventory().getContents()) {
+        				if(ie != null) {
+        					player.getInventory().removeItem(ie);
+        				}
+        			}
+            		player.getInventory().setContents(olympusInv.getContents());
             		player.updateInventory();
             		sender.sendMessage(ChatColor.GREEN + getConfig().getString("Messages.1"));
             	}
